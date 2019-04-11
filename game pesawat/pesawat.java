9a2261f1e4150a5565e47933881aabd41756dc6f
ginanjar21 <47617175+ginanjar21@users.noreply.github.com>
2019-04-10 18:54:48 -0700

    game pesawat

---
+++ game pesawat/pesawat.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pesawat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pesawat extends Actor
+{
+    /**
+     * Act - do whatever the pesawat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (Greenfoot.isKeyDown("left")){
+            turn(-6);
+        }    
+        if (Greenfoot.isKeyDown("right")) {
+            turn(6);
+        }
+        if (Greenfoot.isKeyDown("up")) {
+            move(6);
+        }
+        if (Greenfoot.isKeyDown("down")) {
+            move(-6);
+        }
+        Actor bola= getOneObjectAtOffset(0,0, bola.class);
+        if(bola !=null){
+          getWorld() .removeObject(bola);
+          score.bola++;
+        }
+        if(score.bola == 3){
+            getWorld().addObject(new menang(), 300, 200);
+            Greenfoot.stop();
+        }
+}    }   
